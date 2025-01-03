a7e134e783a255e6156ad662060cad2e67609d4c
kawmini <anuththarakawmini@gmail.com>
2025-01-03 23:10:33 +0530

    add dependency

---
+++ src/styles/Themes.jsx
@@ -0,0 +1,22 @@
+
+
+export const dark = {
+    body:'#202020',
+    text:'#fff',
+    bodyRgba:'32,32,32',
+    textRgba:'255,255,255',
+
+    grey:'#bebebe',
+
+    fontxs :'0.75em',
+    fontxs :'0.875em',
+    fontxs :'1em',
+    fontxs :'1.25em',
+    fontxs :'2em',
+    fontxs :'3em',
+    fontxs :'5em',
+    fontxs :'10em',
+
+    navHeight:'5rem'
+
+}
